1ed20c80e45f7d19dbf0fb7a29695ed8e9686bde
mvayк <mvayk@proton.me>
2025-05-26 22:58:21 +1000

    aqps

--- src/asm/master.asm
+++ src/asm/master.asm
@@ -2,19 +2,47 @@
 ; mvayк
 ; mvayк
 
+; register order:
+; rcx rdx r8 r9
+
 extern printf
+extern MessageBoxA
+
 global asm_main
 
 section .data
-    fmt db "pce", 10, 0 ; 10 is null terminator
+    fmt db "%d", 10, 0
+    success db "pce", 10, 0
+
+    title db "o", 0
+    msg db "msgbox", 0
 
 section .text
 asm_main:
     sub rsp, 40
 
-    lea rcx, [rel fmt] ; rcx is * to string - printf
+    lea rcx, [rel success] ; rcx is * to string - printf
+    call printf
+
+    xor rcx, rcx            ; hwnd
+    lea rdx, [rel msg]      ; text
+    lea r8, [rel title]     ; title
+    mov r9d, 0              ; type
+    call MessageBoxA
+
+    mov rcx, 5
+    mov rdx, 100
+    call add
+
+    mov rcx, fmt
+    mov rdx, rax
     call printf
 
     xor eax, eax ; return 0;
     add rsp, 40 ; realigns the stack
+    ret
+
+add:
+    mov rax, rcx
+    add rax, rdx
     ret
