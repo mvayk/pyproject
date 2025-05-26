d23dccbb7ae4ba03297c0cc701c79bb7c9974b17
mvayк <mvayk@proton.me>
2025-05-27 00:01:23 +1000

    az

--- src/include/include.h
+++ src/include/include.h
@@ -1,1 +1,17 @@
-constexpr void void_void(void);
+/* random file that will contains functions that are not cool enough to be
+   allow in main */
+
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+
+LRESULT CALLBACK wn_proc(HWND hwnd, UINT message, WPARAM w_param, LPARAM l_param) {
+    switch(message) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, message, w_param, l_param);
+    }
+}
