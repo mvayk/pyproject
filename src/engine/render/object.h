e481ffd90ea4604c2de5c02eacd5aef7a770e1cc
mvayк <mvayk@proton.me>
2025-05-28 21:18:54 +1000

    todo: later

---
+++ src/engine/render/object.h
@@ -0,0 +1,16 @@
+#pragma once
+#define GLFW_INCLUDE_NONE
+
+#include <stdio.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+//
+// TODO: later
+//
+
+typedef struct {
+} object;
+
+namespace objects {
+}
---
+++ src/engine/render/render.h
@@ -0,0 +1,7 @@
+#pragma once
+#define GLFW_INCLUDE_NONE
+
+#include <stdio.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
